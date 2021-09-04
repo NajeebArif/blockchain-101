ca3f2fd6c7fc059342cbe5297ed986057e48445d
Najeeb Arif <NajeebArif786@outlook.com>
2021-09-04 21:33:22 +0530

    Update bc101.ts
    Use Union type for blocks to accomodate genesis block.
    
    Add chalk for logging

--- bc101.ts
+++ bc101.ts
@@ -1,4 +1,9 @@
 import * as crypto from 'crypto';
+import * as chalk from 'chalk'
+
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
+const info = chalk.black.bgWhiteBright.bold;
 
 class Block<T>{
     readonly hash: string;
@@ -6,7 +11,7 @@ class Block<T>{
         readonly index: number,
         readonly previousHash: string,
         readonly timestamp: number,
-        readonly data: T
+        readonly data: T | string
     ){
         this.hash = '';
     }
@@ -15,4 +20,32 @@ class Block<T>{
         const data = this.index + this.previousHash + this.timestamp + this.data;
         return crypto.createHash('sha256').update(data).digest('hex');
     }
+}
+
+class Blockchain<T>{
+    private readonly chain: Block<T>[] = [];
+
+    private get latestBlock(): Block<T>{
+        return this.chain[this.chain.length-1];
+    }
+
+    constructor(){
+        const genesisBlock = new Block<T>(0,'0',Date.now(),'Genesis Block');
+        this.chain.push(genesisBlock)
+    }
+
+    addBlock(t: T): void{
+        const block = new Block<T>(this.latestBlock.index+1,this.latestBlock.hash,Date.now(),t);
+        this.chain.push(block);
+    }
+}
+
+export function mine(){
+    const blockchain = new Blockchain<string>();
+    console.log(warning('mining block 1'))
+    blockchain.addBlock('First Block')
+    console.log(warning('mining block 2'))
+    blockchain.addBlock('Second Block')
+
+    console.log(info(JSON.stringify(blockchain)));
 }
