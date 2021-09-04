aff8ce5de9a1cf513abda53b9ddc49ee29f8d3b2
Najeeb Arif <NajeebArif786@outlook.com>
2021-09-04 21:50:59 +0530

    Add Proof of Work while mining
    Add nonce and implement the logic to provide proof of work while mining.

--- bc101.ts
+++ bc101.ts
@@ -7,19 +7,36 @@ const info = chalk.black.bgWhiteBright.bold;
 
 class Block<T>{
     readonly hash: string;
+    readonly nonce: number;
     constructor(
         readonly index: number,
         readonly previousHash: string,
         readonly timestamp: number,
         readonly data: T | string
     ){
-        this.hash = '';
+        const {nonce, hash} = this.mine();
+        this.nonce = nonce;
+        this.hash = hash;
     }
 
-    private calculateHash(): string{
-        const data = this.index + this.previousHash + this.timestamp + this.data;
+    private calculateHash(nonce: number): string{
+        const data = this.index + this.previousHash + this.timestamp + this.data + nonce;
         return crypto.createHash('sha256').update(data).digest('hex');
     }
+
+    private mine(): {nonce: number, hash: string}{
+        let hash: string;
+        let nonce = 0;
+
+        do{
+            hash = this.calculateHash(++nonce);
+        }while(hash.startsWith('00000')===false);
+
+        return {
+            nonce,hash
+        }
+    }
+
 }
 
 class Blockchain<T>{
